5f037c4a1998b77c1616cc5c2ac9c53be57bcb64
NeaEnot <54849648+NeaEnot@users.noreply.github.com>
2021-01-28 21:40:24 +0400

    Update copy_files.py

--- copy_files.py
+++ copy_files.py
@@ -5,12 +5,17 @@ import time
 dir_from = 'Все песни'
 dir_to = 'Плейлист'
 
-try:
-    with open('playlist.txt', 'r', encoding='utf-8') as playlist:
-        for track in playlist:
-            track = track.replace('\n', '')
-            shutil.copy(f'{dir_from}\{track}', f'{dir_to}\{track}')
-            print(track)
-except Exception as e:
-    print(e)
-    time.sleep(10)
+def copy_files(show_log = False):
+    try:
+        with open('playlist.txt', 'r', encoding='utf-8') as playlist:
+            for track in playlist:
+                track = track.replace('\n', '')
+                shutil.copy(f'{dir_from}\{track}', f'{dir_to}\{track}')
+                if show_log:
+                    print(track)
+    except Exception as e:
+        print(e)
+        time.sleep(10)
+
+if __name__ == '__main__':     
+    copy_files(True)
