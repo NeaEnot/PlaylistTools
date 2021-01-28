96ad972fb33ae47c705421a1963f17fe36e1a5cd
NeaEnot <54849648+NeaEnot@users.noreply.github.com>
2021-01-28 21:41:58 +0400

    Update get_all_music.py

--- get_all_music.py
+++ get_all_music.py
@@ -1,17 +1,23 @@
 import os
+import random
 import time
 
 dir_from = 'Все песни'
 
-try:
-    files = os.listdir(dir_from)
-    files.sort()
+def get_all_music(show_log = False):
+    try:
+        files = os.listdir(dir_from)
+        files.sort()
 
-    with open('Музыка.txt', 'w', encoding='utf-8') as file:
-        for track in files:
-            file.write(track + '\n')
-            print(track)
-            
-except Exception as e:
-    print(e)
-    time.sleep(10)
+        with open('Музыка.txt', 'w', encoding='utf-8') as file:
+            for track in files:
+                file.write(track + '\n')
+                if show_log:
+                    print(track)
+                
+    except Exception as e:
+        print(e)
+        time.sleep(10)
+
+if __name__ == '__main__':
+    get_all_music(True)
